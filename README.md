e6885377621797795e5eb319fcd2b9d3e7d8c68a
Foz <fozolo@gmail.com>
2016-07-18 00:46:45 -0400

    Create the justice mod.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Justice Mod
+
+The justice mod provides an system to manage temporary player restrictions.
+A judge privilege enables the 'judge' to convict and sentence other players
+to prison for a specified number of seconds. The mod revokes the convicted
+player's shout, interact, and home privileges and then confines them to
+configurable jail cell locations. The player's position is monitored for the
+duration of their prison sentence to make sure they stay near the cell's
+location. The convicted player is presented with a form explaining the
+procedure, and with a HUD timer to track their time served and indicate when
+they will be released from prison. The time served only counts while the player
+is logged into the server. A record of all convictions is maintained in a
+justice sub-folder within the world directory.
+
+		Examples:
+		/convict Foz 60 arson			-- sentenced to 60 seconds in prison for arson
+		/convict Foz 60 vandalism	-- sentenced to 60 seconds in prison for vandalism
+		/convict Foz 240 murder		-- sentenced to 4 minutes in prison for murder
+		/parole Foz								-- discharge Foz from prison immediately
+
+The mod also detects direct assaults and murders. Offenders are automatically
+convicted and sentenced. PVP zones can be defined to disable the detection
+within a certain area or areas. Further, safe zones can be defined within those
+PVP zones to turn detection back on. This allows for arbitrary and complex
+zoning layouts, such as protected roads or rooms within hostile areas, or PVP
+arenas with safe spectator zones. Zone determination is bases on the victim's
+location rather than the perpetrator's.
---
+++ init.lua
@@ -0,0 +1,482 @@
+-----------------
+-- Justice Mod --
+-----------------
+
+local load_time_start = os.clock()
+
+
+------------
+-- Config --
+------------
+
+local revoke_privs = {'shout', 'interact', 'home'}
+local safe_zones = {}
+local pvp_zones = {
+	{x_min = 524, x_max = 602, y_min = 0, y_max = 76, z_min = 349, z_max = 468}
+}
+local release_pos = {x = 0, y = 22, z = 0}
+local cells = {
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+	{pos = {x = -5, y = 21, z = -4}, occupied = false},
+}
+
+local hud_ids = {}
+local hud_def = {
+	name = 'Prison Sentence',
+	hud_elem_type = 'text',
+	position = {x=0.5, y=0.75},
+	scale = {x=1000, y=1000},
+	text = 'Time served: 0/?',
+	number = 0xFF0000,
+	alignment = {x=0, y=0},
+	offset = {x=0, y=0},
+}
+
+
+---------------------
+-- Data Management --
+---------------------
+
+local data_file = core.get_worldpath() .. 'justice/data.mt'
+local data = {}
+
+local function load_data_file()
+	local file = io.open(data_file, "r")
+	if file then
+		data = core.deserialize(file:read("*all"))
+		file:close()
+	end
+end
+
+local function write_data_file()
+	local file = io.open(data_file, 'w')
+	if file then
+		file:write(core.serialize(data))
+		file:close()
+	end
+end
+
+-- Save the data when the server shutdown.
+core.register_on_shutdown(function()
+	write_data_file()
+end)
+
+load_data_file()
+data.records = data.records or {}
+data.inmates = data.inmates or {}
+data.inmates.active   = data.inmates.active   or {}
+data.inmates.inactive = data.inmates.inactive or {}
+
+-- There are no logged in players when the server starts, so deactivate all
+-- inmates when the server first starts up. This should have been handled during
+-- shutdown, but the server may have not have stopped gracefully.
+for _,inmate in ipairs(data.inmates.active) do
+	table.insert(data.inmates.inactive, {
+		player_name = inmate.player_name,
+		sentence    = inmate.sentence or 60,
+		time_served = inmate.time_served or 0,
+		--cell_number = nil, -- does nothing
+	})
+end
+data.inmates.active = {}
+write_data_file()
+
+
+----------------------
+-- Helper Functions --
+----------------------
+
+local function round(number)
+	-- Round numbers to the nearest integer. Numbers exactly between 
+	-- two integers (ending in .5) are rounded away from zero.
+	if number >= 0.5 then
+		return math.floor(number + 0.5)
+	else 
+		return math.ceil(number - 0.5)
+	end
+end
+
+local function find_free_pos_near(pos)
+	local x, y, z, r = pos.x, pos.y, pos.z, 1
+	for j = y-0, y+r do
+		for i = x-r, x+r do
+			for k = z-r, z+r do
+				local p1 = {x=i, y=j,   z=k}
+				local p2 = {x=i, y=j+1, z=k}
+				local n1 = core.get_node(p1)
+				local n2 = core.get_node(p2)
+				local walkable
+				if not core.registered_nodes[n1.name].walkable and
+				   not core.registered_nodes[n2.name].walkable then
+					return p1, true
+				end
+			end
+		end
+	end
+	return pos, false
+end
+
+local function find_free_cell()
+	for i, cell in ipairs(cells) do
+		if not cell.occupied then
+			return i, cell
+		end
+	end
+	return 1, cells[1]
+end
+
+local function in_safe_zone(pos)
+	local x, y, z = pos.x, pos.y, pos.z
+	for _, zone in ipairs(safe_zones) do
+		if zone.x_min <= x and x <= zone.x_max and
+		   zone.y_min <= y and y <= zone.y_max and
+		   zone.z_min <= z and z <= zone.z_max then
+			return true
+		end
+	end
+
+	for _, zone in ipairs(pvp_zones) do
+		if zone.x_min <= x and x <= zone.x_max and
+		   zone.y_min <= y and y <= zone.y_max and
+		   zone.z_min <= z and z <= zone.z_max then
+			return false
+		end
+	end
+
+	return true
+end
+
+local function revoke(name)
+	local privs = core.get_player_privs(name)
+	if not privs then 
+		core.log('warning', 'Unable to revoke ' .. name .. '\'s privileges.')
+		return false
+	end
+
+	for _,priv in ipairs(revoke_privs) do
+    privs[priv] = nil
+  end
+
+	core.set_player_privs(name, privs)
+	return true
+end
+
+local function grant(name)
+	local privs = core.get_player_privs(name)
+	if not privs then
+		core.log('warning', 'Unable to restore ' .. name .. '\'s privileges.')
+		return false
+	end
+
+	for _,priv in ipairs(revoke_privs) do
+    privs[priv] = true
+  end
+
+	core.set_player_privs(name, privs)
+	return true
+end
+
+local function confine(inmate)
+	local player = core.get_player_by_name(inmate.name)
+
+	if not player then
+		core.log('warning', 'Unable to confine ' .. inmate.name .. ' to prison.')
+		return false
+	end
+
+	local cell_number, cell = find_free_cell()
+	inmate.cell_number = cell_number
+	cell.occupied = true
+	player:setpos(cell.pos)
+
+	local def = hud_def
+	def.text = 'Time Served: ' ..
+		tostring(round(inmate.time_served)) .. '/' ..
+		tostring(round(inmate.sentence)) .. '(s)'
+	hud_ids[inmate.name] = player:hud_add(def)
+
+	core.sound_play("teleport", {
+		to_player = inmate.name,
+		gain = 0.1
+	})
+
+	return true
+end
+
+local function release(inmate)
+	local player = core.get_player_by_name(inmate.name)
+
+	if not player then
+		core.log('warning', 'Unable to release ' .. inmate.name .. ' from prison.')
+		return false
+	end
+
+	cells[inmate.cell_number].occupied = false
+	player:setpos(find_free_pos_near(release_pos))
+
+	player:hud_remove(hud_ids[inmate.name])
+	hud_ids[inmate.name] = nil
+
+	core.sound_play("teleport", {
+		to_player = inmate.name,
+		gain = 0.1
+	})
+
+	return true
+end
+
+
+---------
+-- API --
+---------
+justice = {}
+
+function justice.sentence(judge, player_name, seconds, cause)
+	local record = {
+		date = os.date('%Y:%m:%d %X'),
+		judge = judge,
+		duration = seconds,
+		cause = cause,
+	}
+	local inmate = {
+		name = player_name,
+		sentence = seconds,
+		time_served = 0,
+	}
+
+	if confine(inmate) and revoke(inmate.name) then
+		if not data.records[inmate.name] then
+			data.records[inmate.name] = {}
+		end
+		table.insert(data.records[inmate.name], record)
+
+		if not data.inmates.active[inmate.name] then
+			data.inmates.active[inmate.name] = {}
+		end
+		data.inmates.active[inmate.name] = inmate
+
+		write_data_file()
+
+		core.log('action', judge .. ' sentenced ' .. inmate.name .. ' to ' ..
+			tostring(seconds) .. ' seconds in prison for ' .. cause .. '.')
+		core.chat_send_all(inmate.name ..	' has been found guilty of ' .. cause ..
+			' and has been sentenced to prison for ' .. tostring(seconds) ..
+			' seconds.')
+		local formspec = 'size[8,9]'..
+			'textarea[0.3,0.25;8,9;court;= The Court of FozLand =;'..
+			'\nYou have been tried and found you guilty of ' .. cause .. '. \n\n' ..
+			'You are hereby sentenced to prison for ' .. tostring(seconds) .. 
+			' seconds. \n\nYour privileges have been reduced while you serve your ' ..
+			' sentence. Please review the rules at /news if you have any questions]'..
+			'button_exit[5.5,8.4;2.5,1;exit;I Understand]'
+		core.show_formspec(inmate.name, "Conviction", formspec)
+	else
+		local notice = 'Sentencing ' .. player_name .. ' failed.'
+		core.chat_send_player(judge, notice)
+		core.log('warning', notice)
+	end
+end
+
+function justice.discharge(judge, player_name)
+
+	local inmate = data.inmates.active[player_name]
+
+	if inmate and release(inmate) and grant(inmate.name) then
+		local notice = inmate.name ..	' has completed their sentence. They have' ..
+			' been released from prison and their privileges have been restored.'
+		core.log('action', notice)
+		core.chat_send_all(notice)
+
+		data.inmates.active[player_name] = nil
+		write_data_file()
+	else
+		local notice = 'Paroling ' .. player_name .. ' failed.'
+		core.chat_send_player(judge, notice)
+		core.log('warning', notice)
+	end
+end
+
+
+--------------------
+-- User Interface --
+--------------------
+
+core.register_privilege(
+	'judge',
+	'Allows player to sentence others to prison.'
+)
+
+core.register_chatcommand('convict', {
+	params = '<name> <seconds> <reason>',
+	description = 'Revokes a players interact and shout privileges and ' ..
+		'confines them to prison for the specified number of seconds.',
+	privs = {judge=true},
+	func = function(judge_name, param)
+		local player_name, seconds, cause =
+			string.match(param, '^([^ ]+) (%d+) (.+)$')
+		if not player_name or not seconds or not cause then
+			return false, 'Invalid parameters (see /help convict)'
+		elseif not core.auth_table[player_name] then
+			return false, 'Player ' .. player_name .. ' does not exist.'
+		end
+		justice.sentence(judge_name, player_name, tonumber(seconds), cause)
+	end,
+})
+
+core.register_chatcommand('parole', {
+	params = '<name>',
+	description = 'Release a convict from prison and restore their privileges.',
+	privs = {judge=true},
+	func = function(judge, param)
+		local player_name = string.match(param, '^([^ ]+)$')
+		if not player_name then
+			return false, 'Invalid parameters (see /help parole)'
+		elseif not core.auth_table[player_name] then
+			return false, 'Player ' .. player_name .. ' does not exist.'
+		end
+		justice.discharge(judge, player_name)
+	end,
+})
+
+
+----------
+-- Core --
+----------
+
+-- Detect unsanctioned violence and punish the perpetrators.
+core.register_on_punchplayer(
+	function(victim, hitter, time_from_last_punch, tool_capabilities, dir, damage)
+		if in_safe_zone(victim:getpos()) then
+			if damage >= victim:get_hp() then
+				justice.sentence('The court', hitter:get_player_name(), 240, 'murder')
+			elseif time_from_last_punch < 2 then
+				justice.sentence('The court', hitter:get_player_name(), 30, 'assault')
+			end
+		end
+	end
+)
+
+-- Let inmates who die in custody respawn in a prison cell.
+core.register_on_respawnplayer(function(player)
+	local name = player:get_player_name()
+	if data.inmates.active[name] then
+		local inmate = data.inmates.active[name]
+		local cell = cells[inmate.cell_number]
+		player:setpos(cell.pos)
+	end
+	return true -- Disable regular player placement.
+end)
+
+-- Move inmate players who log out, to the inactive inmates table.
+core.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	if data.inmates.active[name] then
+		-- Mark the inmates cell as unoccupied.
+		local inmate = data.inmates.active[name]
+		cells[inmate.cell_number].occupied = false
+
+		-- Move the inmate to the inactive list.
+		data.inmates.inactive[name] = inmate
+		data.inmates.active[name]   = nil
+		write_data_file()
+	end
+end)
+
+-- Move inmate players to the active inmates table when they log in.
+core.register_on_joinplayer(function(player)
+	local name = player:get_player_name()
+	if data.inmates.inactive[name] then
+		-- Find an unoccupied cell for the inmate.
+		local inmate = data.inmates.inactive[name]
+		local cell_number, cell = find_free_cell()
+		inmate.cell_number = cell_number
+		cell.occupied = true
+		player:setpos(cell.pos)
+		
+		-- Move the inmate to the active list.
+		data.inmates.active[name]   = inmate
+		data.inmates.inactive[name] = nil
+		write_data_file()
+	end
+end)
+
+local function update_hud(inmate)
+	local player = core.get_player_by_name(inmate.name)
+	local status = 'Time Served: ' ..
+		tostring(round(inmate.time_served)) .. '/' ..
+		tostring(round(inmate.sentence)) .. '(s)'
+
+	if hud_ids[inmate.name] then
+		player:hud_change(tonumber(hud_ids[inmate.name]), 'text', status)
+	else
+		core.log('warning','Justice mod missing hud id for player ' ..
+			inmate.name .. '.')
+
+		local def = hud_def
+		def.text = status
+		hud_ids[inmate.name] = player:hud_add(def)
+	end
+end
+
+-- Track time served for active (logged-in) inmates.
+local time = 0
+local count = 0
+core.register_globalstep(function(dtime)
+	time = time + dtime
+	if time > 1 then -- about every second
+		for name, inmate in pairs(data.inmates.active) do
+			local player = core.get_player_by_name(name)
+
+			-- Check for escapees, return them to prison and double their sentence.
+			local p1 = cells[inmate.cell_number].pos
+			local p2 = player:getpos()
+			if vector.distance(p1,p2) > 5 then
+				inmate.sentence = 2 * inmate.sentence
+				player:setpos(p1)
+			end
+
+			-- Check for inmates with completed sentences and discharge them.
+			inmate.time_served = inmate.time_served + time
+			if inmate.time_served >= inmate.sentence then
+				justice.discharge('The court', name)
+			else -- Update the HUD timer.
+				update_hud(inmate)
+			end
+		end
+		time = 0
+
+		--[[
+		-- save data every 5 minutes in case of a server crash
+		count = count + 1
+		if count > 300 then
+			write_data_file()
+		end
+		--]]
+	end
+end)
+
+
+core.log(
+	'action',
+	string.format(
+		'['..core.get_current_modname()..'] loaded in %.3fs',
+		os.clock() - load_time_start
+	)
+)
